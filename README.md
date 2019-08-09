69fb915db904303dad6d844f52ad19e7d942d485
Daniel Vivar <jose.daniel.vivar.personat@ing.com>
2019-08-09 14:27:41 +0200

    Added Readme and Standard

---
+++ README.md
@@ -0,0 +1,13 @@
+# ING Responsive Menu (WIP)
+
+Install dependencies
+
+```bash
+npm i
+```
+
+Start
+
+```bash
+npm run start
+```
--- src/bottom-of-body.js
+++ src/bottom-of-body.js
@@ -1,33 +1,33 @@
-import "./styles.scss";
-import "@fortawesome/fontawesome-free/js/all.js";
+import './styles.scss'
+import '@fortawesome/fontawesome-free/js/all.js'
 
-let delay = 400;
-let animationTime = 1000;
+const delay = 400
+const animationTime = 1000
 
 window.openMenu = () => {
-  console.log("openMenu");
-  const menuClosed = document.getElementById("menu--closed");
-  const menuTransitioning = document.getElementById("menu--transitioning");
-  const menuOpened = document.getElementById("menu--opened");
+  console.log('openMenu')
+  const menuClosed = document.getElementById('menu--closed')
+  const menuTransitioning = document.getElementById('menu--transitioning')
+  const menuOpened = document.getElementById('menu--opened')
 
-  menuClosed.classList.toggle("--hidden");
-  menuTransitioning.classList.toggle("--shown");
+  menuClosed.classList.toggle('--hidden')
+  menuTransitioning.classList.toggle('--shown')
   setTimeout(() => {
-    menuOpened.classList.toggle("--shown");
-  }, delay);
-};
+    menuOpened.classList.toggle('--shown')
+  }, delay)
+}
 
 window.closeMenu = () => {
-  console.log("closeMenu");
-  const menuClosed = document.getElementById("menu--closed");
-  const menuTransitioning = document.getElementById("menu--transitioning");
-  const menuOpened = document.getElementById("menu--opened");
+  console.log('closeMenu')
+  const menuClosed = document.getElementById('menu--closed')
+  const menuTransitioning = document.getElementById('menu--transitioning')
+  const menuOpened = document.getElementById('menu--opened')
 
-  menuOpened.classList.toggle("--shown");
-  menuTransitioning.classList.toggle("--shown");
+  menuOpened.classList.toggle('--shown')
+  menuTransitioning.classList.toggle('--shown')
   setTimeout(() => {
-    menuClosed.classList.toggle("--hidden");
-  }, animationTime);
-};
+    menuClosed.classList.toggle('--hidden')
+  }, animationTime)
+}
 
-document.body.style.display = "";
+document.body.style.display = ''
